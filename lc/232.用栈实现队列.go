c76e542ea8e849ba5c5ac53e559ced31d1e1889f
fakeYanss <yanshisangc@gmail.com>
2022-07-10 23:29:26 +0800

    232 🚩

--- lc/232.用栈实现队列.go
+++ lc/232.用栈实现队列.go
@@ -14,35 +14,35 @@
  * Testcase Example:  '["MyQueue","push","push","peek","pop","empty"]\n[[],[1],[2],[],[],[]]'
  *
  * 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
- * 
+ *
  * 实现 MyQueue 类：
- * 
- * 
+ *
+ *
  * void push(int x) 将元素 x 推到队列的末尾
  * int pop() 从队列的开头移除并返回元素
  * int peek() 返回队列开头的元素
  * boolean empty() 如果队列为空，返回 true ；否则，返回 false
- * 
- * 
+ *
+ *
  * 说明：
- * 
- * 
+ *
+ *
  * 你 只能 使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty
  * 操作是合法的。
  * 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：
  * ["MyQueue", "push", "push", "peek", "pop", "empty"]
  * [[], [1], [2], [], [], []]
  * 输出：
  * [null, null, null, 1, 1, false]
- * 
+ *
  * 解释：
  * MyQueue myQueue = new MyQueue();
  * myQueue.push(1); // queue is: [1]
@@ -50,61 +50,73 @@
  * myQueue.peek(); // return 1
  * myQueue.pop(); // return 1, queue is [2]
  * myQueue.empty(); // return false
- * 
- * 
- * 
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 1 <= x <= 9
  * 最多调用 100 次 push、pop、peek 和 empty
  * 假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 进阶：
- * 
- * 
+ *
+ *
  * 你能否实现每个操作均摊时间复杂度为 O(1) 的队列？换句话说，执行 n 个操作的总时间复杂度为 O(n) ，即使其中一个操作可能花费较长时间。
- * 
- * 
+ *
+ *
  */
+package lc
 
 // @lc code=start
 type MyQueue struct {
-
+	s1 []int
+	s2 []int
 }
-
 
 func Constructor() MyQueue {
-
+	return MyQueue{[]int{}, []int{}}
 }
 
-
-func (this *MyQueue) Push(x int)  {
-
+func (q *MyQueue) Push(x int) {
+	q.s1 = append(q.s1, x)
 }
 
-
-func (this *MyQueue) Pop() int {
-
+func (q *MyQueue) Pop() int {
+	lenS2 := len(q.s2)
+	if lenS2 == 0 {
+		q.move()
+	}
+	res := q.s2[lenS2-1]
+	q.s2 = q.s2[:lenS2-1]
+	return res
 }
 
-
-func (this *MyQueue) Peek() int {
-
+func (q *MyQueue) move() {
+	for len(q.s1) > 0 {
+		q.s2 = append(q.s2, q.s1[len(q.s1)-1])
+		q.s1 = q.s1[:len(q.s1)-1]
+	}
 }
 
-
-func (this *MyQueue) Empty() bool {
-
+func (q *MyQueue) Peek() int {
+	if len(q.s2) == 0 {
+		q.move()
+	}
+	return q.s2[len(q.s2)-1]
 }
 
+func (q *MyQueue) Empty() bool {
+	return len(q.s1) == 0 && len(q.s2) == 0
+}
 
 /**
  * Your MyQueue object will be instantiated and called as such:
@@ -115,4 +127,3 @@ func (this *MyQueue) Empty() bool {
  * param_4 := obj.Empty();
  */
 // @lc code=end
-
