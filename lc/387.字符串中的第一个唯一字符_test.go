42f963073fb14ce4c8cbeed3af5ae82daa09bb72
fakeYanss <yanshisangc@gmail.com>
2022-07-15 00:25:22 +0800

    387 🚩

---
+++ lc/387.字符串中的第一个唯一字符_test.go
@@ -0,0 +1,25 @@
+package lc
+
+import "testing"
+
+func Test_firstUniqChar(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"case1", args{"leetcode"}, 0},
+		{"case2", args{"loveleetcode"}, 2},
+		{"case1", args{"aabb"}, -1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := firstUniqChar(tt.args.s); got != tt.want {
+				t.Errorf("firstUniqChar() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
