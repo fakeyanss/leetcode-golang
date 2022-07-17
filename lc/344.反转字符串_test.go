ce35f8bb57ae2775d80bec0b2e5ee6172334192f
fakeYanss <yanshisangc@gmail.com>
2022-07-17 23:01:45 +0800

    344 🚩

---
+++ lc/344.反转字符串_test.go
@@ -0,0 +1,26 @@
+package lc
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_reverseString(t *testing.T) {
+	type args struct {
+		s []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{"case1", args{[]byte{'h', 'e', 'l', 'l', 'o'}}, []byte{'o', 'l', 'l', 'e', 'h'}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if reverseString(tt.args.s); !reflect.DeepEqual(tt.args.s, tt.want) {
+				t.Errorf("reverseString() = %v, want %v", tt.args.s, tt.want)
+			}
+		})
+	}
+}
