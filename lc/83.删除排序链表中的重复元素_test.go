c58c68ff4090150e7a990e89c5bd3f29b46294ea
fakeYanss <yanshisangc@gmail.com>
2022-07-17 16:07:55 +0800

    83 🚩

---
+++ lc/83.删除排序链表中的重复元素_test.go
@@ -0,0 +1,27 @@
+package lc
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_deleteDuplicates(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{"case1", args{NewListNode(1, NewListNode(1, NewListNode(2, nil)))}, NewListNode(1, NewListNode(2, nil))},
+		{"case1", args{NewListNode(1, NewListNode(1, NewListNode(1, NewListNode(2, NewListNode(3, NewListNode(3, nil))))))}, NewListNode(1, NewListNode(2, NewListNode(3, nil)))},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := deleteDuplicates(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("deleteDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
