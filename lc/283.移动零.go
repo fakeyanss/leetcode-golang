8ac5973c46bca8225dc0ecaeee762762e3bcd2ad
fakeYanss <yanshisangc@gmail.com>
2022-07-17 22:08:57 +0800

    283 🚩

---
+++ lc/283.移动零.go
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=golang
+ *
+ * [283] 移动零
+ *
+ * https://leetcode.cn/problems/move-zeroes/description/
+ *
+ * algorithms
+ * Easy (64.04%)
+ * Likes:    1656
+ * Dislikes: 0
+ * Total Accepted:    807K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[0,1,0,3,12]'
+ *
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ *
+ * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: nums = [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: nums = [0]
+ * 输出: [0]
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ *
+ * 1 <= nums.length <= 10^4
+ * -2^31 <= nums[i] <= 2^31 - 1
+ *
+ *
+ *
+ *
+ * 进阶：你能尽量减少完成的操作次数吗？
+ *
+ */
+package lc
+
+// @lc code=start
+func moveZeroes(nums []int) {
+	// 第27题，先移除0, 返回剩余的长度
+	slow, fast := 0, 0
+	length := len(nums)
+	for fast < length {
+		if nums[fast] != 0 {
+			nums[slow] = nums[fast]
+			slow++
+		}
+		fast++
+	}
+
+	// 再将末尾的数字替换为0
+	for i := slow; i < length; i++ {
+		nums[i] = 0
+	}
+}
+
+// @lc code=end
---
+++ lc/283.移动零_test.go
@@ -0,0 +1,26 @@
+package lc
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_moveZeroes(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"case1", args{[]int{0, 1, 0, 3, 12}}, []int{1, 3, 12, 0, 0}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if moveZeroes(tt.args.nums); !reflect.DeepEqual(tt.args.nums, tt.want) {
+				t.Errorf("moveZeroes() = %v, want %v", tt.args.nums, tt.want)
+			}
+		})
+	}
+}
