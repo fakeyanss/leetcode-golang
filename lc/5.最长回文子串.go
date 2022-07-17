28c79a9e08b1d7d0a33f1eb9ed74fc7a03a77640
fakeYanss <yanshisangc@gmail.com>
2022-07-17 23:49:28 +0800

    5 🚩

---
+++ lc/5.最长回文子串.go
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=golang
+ *
+ * [5] 最长回文子串
+ *
+ * https://leetcode.cn/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (36.88%)
+ * Likes:    5465
+ * Dislikes: 0
+ * Total Accepted:    1.1M
+ * Total Submissions: 3M
+ * Testcase Example:  '"babad"'
+ *
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= s.length <= 1000
+ * s 仅由数字和英文字母组成
+ *
+ *
+ */
+package lc
+
+// @lc code=start
+func longestPalindrome(s string) string {
+	// 中心扩散
+	res := ""
+	for i := 0; i < len(s); i++ {
+		// 以 s[i] 为中心的最长回文子串
+		s1 := palindrome(s, i, i)
+		// 以 s[i] 和 s[i+1] 为中心的最长回文子串
+		s2 := palindrome(s, i, i+1)
+		if len(s1) > len(res) {
+			res = s1
+		}
+		if len(s2) > len(res) {
+			res = s2
+		}
+	}
+	return res
+}
+
+func palindrome(s string, l int, r int) string {
+	for l >= 0 && r < len(s) && s[l] == s[r] {
+		l--
+		r++
+	}
+	return s[l+1 : r]
+}
+
+// @lc code=end
