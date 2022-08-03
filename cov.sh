dbd3b5ac1faab572dea344693b6fd8b6ebd248dc
fakeYanss <yanshisangc@gmail.com>
2022-08-04 00:00:43 +0800

    ut

--- cov.sh
+++ cov.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-go test -coverprofile=cov.data -coverpkg=./lc ./lc
+# go test -coverprofile=cov.out -coverpkg=./lc ./lc
+go test -covermode=count -coverprofile=count.out -coverpkg=./lc ./lc
 
-go tool cover -html=cov.data -o cov.html
+# go tool cover -html=cov.data -o cov.html
+go tool cover -func=count.out|grep -v '100.0%'
 
-open cov.html
+# open cov.html
