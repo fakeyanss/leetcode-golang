3834eb3720e640b1d974090f3e160df2da70d9ef
fakeYanss <yanshisangc@gmail.com>
2022-08-06 23:49:54 +0800

    ut

---
+++ lc/54.螺旋矩阵_test.go
@@ -0,0 +1,26 @@
+package lc
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_spiralOrder(t *testing.T) {
+	type args struct {
+		matrix [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"case1", args{[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}}, []int{1, 2, 3, 6, 9, 8, 7, 4, 5}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := spiralOrder(tt.args.matrix); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("spiralOrder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ lc/567.字符串的排列_test.go
@@ -0,0 +1,25 @@
+package lc
+
+import "testing"
+
+func Test_checkInclusion(t *testing.T) {
+	type args struct {
+		s1 string
+		s2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"case1", args{"ab", "eidbaooo"}, true},
+		{"case2", args{"ab", "eidboaoo"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := checkInclusion(tt.args.s1, tt.args.s2); got != tt.want {
+				t.Errorf("checkInclusion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
