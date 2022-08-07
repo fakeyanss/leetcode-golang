3fa66ef2994acf199df4832145e69d19e8483d95
fakeYanss <yanshisangc@gmail.com>
2022-08-07 12:29:18 +0800

    187 🚩

---
+++ lc/187.重复的dna序列_test.go
@@ -0,0 +1,26 @@
+package lc
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_findRepeatedDnaSequences(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{"case1", args{"AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"}, []string{"AAAAACCCCC", "CCCCCAAAAA"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findRepeatedDnaSequences(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findRepeatedDnaSequences() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
