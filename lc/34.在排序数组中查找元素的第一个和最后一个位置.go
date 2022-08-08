2da0c59f99211a39ed85fc0154bb0d3a1ef4138c
fakeYanss <yanshisangc@gmail.com>
2022-08-08 23:08:16 +0800

    34 🚩

---
+++ lc/34.在排序数组中查找元素的第一个和最后一个位置.go
@@ -0,0 +1,101 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=golang
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ *
+ * https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (42.30%)
+ * Likes:    1841
+ * Dislikes: 0
+ * Total Accepted:    610.6K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+ *
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ *
+ * 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [5,7,7,8,8,10], target = 8
+ * 输出：[3,4]
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [5,7,7,8,8,10], target = 6
+ * 输出：[-1,-1]
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：nums = [], target = 0
+ * 输出：[-1,-1]
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 0 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ * nums 是一个非递减数组
+ * -10^9 <= target <= 10^9
+ *
+ *
+ */
+package lc
+
+// @lc code=start
+func searchRange(nums []int, target int) []int {
+	return []int{binaryLeft(nums, target), binaryRight(nums, target)}
+}
+
+func binaryLeft(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			// 找到值，开始找左边界
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		}
+	}
+	// 检查left越界情况
+	if left >= len(nums) || nums[left] != target {
+		return -1
+	}
+	return left
+}
+
+func binaryRight(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			// 找到值，开始找右边界
+			left = mid + 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		}
+	}
+	// 检查right越界情况
+	if right < 0 || nums[right] != target {
+		return -1
+	}
+	return right
+}
+
+// @lc code=end
