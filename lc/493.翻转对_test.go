f0a935d8093080e4c5e40f21eeabbe42e5ed11d6
fakeYanss <yanshisangc@gmail.com>
2022-09-01 00:11:09 +0800

    493 🚩

---
+++ lc/493.翻转对_test.go
@@ -0,0 +1,23 @@
+package lc
+
+import "testing"
+
+func Test_reversePairs(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"case1", args{[]int{2, 4, 3, 5, 1}}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reversePairs(tt.args.nums); got != tt.want {
+				t.Errorf("reversePairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
