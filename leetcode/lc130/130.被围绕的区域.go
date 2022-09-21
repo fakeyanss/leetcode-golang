00473926cfb029e8a8075c025c28a88e995f12fa
fakeYanss <yanshisangc@gmail.com>
2022-09-22 01:24:06 +0800

    岛屿问题 - 130 🚩

---
+++ leetcode/lc130/130.被围绕的区域.go
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode.cn id=130 lang=golang
+ *
+ * [130] 被围绕的区域
+ *
+ * https://leetcode.cn/problems/surrounded-regions/description/
+ *
+ * algorithms
+ * Medium (45.97%)
+ * Likes:    869
+ * Dislikes: 0
+ * Total Accepted:    199.5K
+ * Total Submissions: 433.8K
+ * Testcase Example:  '[["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]'
+ *
+ * 给你一个 m x n 的矩阵 board ，由若干字符 'X' 和 'O' ，找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X'
+ * 填充。
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：board =
+ * [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+ * 输出：[["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+ * 解释：被围绕的区间不会存在于边界上，换句话说，任何边界上的 'O' 都不会被填充为 'X'。 任何不在边界上，或不与边界上的 'O' 相连的 'O'
+ * 最终都会被填充为 'X'。如果两个元素在水平或垂直方向相邻，则称它们是“相连”的。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：board = [["X"]]
+ * 输出：[["X"]]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * m == board.length
+ * n == board[i].length
+ * 1
+ * board[i][j] 为 'X' 或 'O'
+ *
+ *
+ *
+ *
+ */
+package lc130
+
+// @lc code=start
+func solve(board [][]byte) {
+	if len(board) == 0 || len(board[0]) == 0 {
+		return
+	}
+	m, n := len(board), len(board[0])
+	// 淹掉左右两边的半岛
+	for i := 0; i < m; i++ {
+		dfs(board, i, 0)
+		dfs(board, i, n-1)
+	}
+
+	// 淹掉左右两边的半岛
+	for i := 0; i < n; i++ {
+		dfs(board, 0, i)
+		dfs(board, m-1, i)
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if board[i][j] == 'P' {
+				board[i][j] = 'O'
+			} else if board[i][j] == 'O' {
+				board[i][j] = 'X'
+			}
+		}
+	}
+}
+
+func dfs(board [][]byte, i, j int) {
+	m, n := len(board), len(board[0])
+	if i < 0 || i >= m || j < 0 || j >= n {
+		return
+	}
+	if board[i][j] != 'O' {
+		return
+	}
+	board[i][j] = 'P'
+	dfs(board, i-1, j)
+	dfs(board, i+1, j)
+	dfs(board, i, j-1)
+	dfs(board, i, j+1)
+}
+
+// @lc code=end
