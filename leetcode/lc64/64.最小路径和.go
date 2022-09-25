3e89300aa71d478ebdac42f0fb828ab1177cd13f
fakeYanss <yanshisangc@gmail.com>
2022-09-25 23:27:53 +0800

    动态规划-最小路径和 - 64 🚩

---
+++ leetcode/lc64/64.最小路径和.go
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode.cn id=64 lang=golang
+ *
+ * [64] 最小路径和
+ *
+ * https://leetcode.cn/problems/minimum-path-sum/description/
+ *
+ * algorithms
+ * Medium (69.33%)
+ * Likes:    1355
+ * Dislikes: 0
+ * Total Accepted:    419.4K
+ * Total Submissions: 604.8K
+ * Testcase Example:  '[[1,3,1],[1,5,1],[4,2,1]]'
+ *
+ * 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ *
+ * 说明：每次只能向下或者向右移动一步。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
+ * 输出：7
+ * 解释：因为路径 1→3→1→1→1 的总和最小。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：grid = [[1,2,3],[4,5,6]]
+ * 输出：12
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * m == grid.length
+ * n == grid[i].length
+ * 1
+ * 0
+ *
+ *
+ */
+package lc64
+
+import "math"
+
+// @lc code=start
+var memo [][]int
+
+func minPathSum(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	memo = make([][]int, m)
+	for i := range memo {
+		memo[i] = make([]int, n)
+	}
+	return dp(grid, m-1, n-1)
+}
+
+// dp(i, j) = x，表示从左上角到grid[i][j]的最小路径和为x
+// dp(i, j) = min(dp(i-1, j), dp(i, j-1)) + grid[i][j]
+func dp(grid [][]int, i, j int) int {
+	if i == 0 && j == 0 {
+		return grid[0][0]
+	}
+	if i < 0 || j < 0 {
+		return math.MaxInt
+	}
+	if memo[i][j] != 0 {
+		return memo[i][j]
+	}
+	memo[i][j] = minInt(dp(grid, i-1, j), dp(grid, i, j-1)) + grid[i][j]
+	return memo[i][j]
+}
+
+func minInt(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// @lc code=end
