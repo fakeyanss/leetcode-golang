969310f168dc66b73d1c30f266fca4dfd5066fb0
fakeYanss <yanshisangc@gmail.com>
2022-09-30 09:05:17 +0800

    动态规划-股票买卖 - 309 🚩

---
+++ leetcode/lc309/309.最佳买卖股票时机含冷冻期.go
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode.cn id=309 lang=golang
+ *
+ * [309] 最佳买卖股票时机含冷冻期
+ *
+ * https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-with-cooldown/description/
+ *
+ * algorithms
+ * Medium (63.73%)
+ * Likes:    1326
+ * Dislikes: 0
+ * Total Accepted:    212K
+ * Total Submissions: 332.7K
+ * Testcase Example:  '[1,2,3,0,2]'
+ *
+ * 给定一个整数数组prices，其中第  prices[i] 表示第 i 天的股票价格 。​
+ *
+ * 设计一个算法计算出最大利润。在满足以下约束条件下，你可以尽可能地完成更多的交易（多次买卖一支股票）:
+ *
+ *
+ * 卖出股票后，你无法在第二天买入股票 (即冷冻期为 1 天)。
+ *
+ *
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: prices = [1,2,3,0,2]
+ * 输出: 3
+ * 解释: 对应的交易状态为: [买入, 卖出, 冷冻期, 买入, 卖出]
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: prices = [1]
+ * 输出: 0
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= prices.length <= 5000
+ * 0 <= prices[i] <= 1000
+ *
+ *
+ */
+package lc309
+
+// @lc code=start
+func maxProfit(prices []int) int {
+	return dp(prices)
+}
+
+// dp[i][k][0 or 1]
+// 0 <= i <= n - 1, 1 <= k <= K
+// n 为天数，大 K 为交易数的上限，0 和 1 代表是否持有股票。
+// 此问题共 n × K × 2 种状态，全部穷举就能搞定。
+
+// base case：
+// dp[-1][...][0] = dp[...][0][0] = 0
+// dp[-1][...][1] = dp[...][0][1] = -infinity
+
+// 状态转移方程：
+// dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
+// dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i])
+func dp(prices []int) int {
+	// k正无穷，且卖出后不能第二天买入
+	n := len(prices)
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, 2)
+	}
+	for i := 0; i < n; i++ {
+		if i-1 == -1 {
+			dp[i][0] = 0
+			dp[i][1] = -prices[i]
+			continue
+		}
+		if i-2 == -1 {
+			dp[i][0] = maxInt(dp[i-1][0], dp[i-1][1]+prices[i])
+			dp[i][1] = maxInt(dp[i-1][1], -prices[i])
+			continue
+		}
+		dp[i][0] = maxInt(dp[i-1][0], dp[i-1][1]+prices[i])
+		dp[i][1] = maxInt(dp[i-1][1], dp[i-2][0]-prices[i])
+	}
+	return dp[n-1][0]
+}
+
+func maxInt(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// @lc code=end
