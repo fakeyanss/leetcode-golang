c1e4ba222bdb846ae591bd8f50feaa9a718650d7
fakeYanss <yanshisangc@gmail.com>
2022-11-28 17:39:08 +0800

    二叉树遍历-二叉堆特性 - 662 🚩

---
+++ leetcode/lc662/662.二叉树最大宽度.go
@@ -0,0 +1,166 @@
+/*
+ * @lc app=leetcode.cn id=662 lang=golang
+ *
+ * [662] 二叉树最大宽度
+ *
+ * https://leetcode.cn/problems/maximum-width-of-binary-tree/description/
+ *
+ * algorithms
+ * Medium (43.63%)
+ * Likes:    522
+ * Dislikes: 0
+ * Total Accepted:    80.8K
+ * Total Submissions: 185.3K
+ * Testcase Example:  '[1,3,2,5,3,null,9]'
+ *
+ * 给你一棵二叉树的根节点 root ，返回树的 最大宽度 。
+ *
+ * 树的 最大宽度 是所有层中最大的 宽度 。
+ *
+ *
+ *
+ * 每一层的 宽度 被定义为该层最左和最右的非空节点（即，两个端点）之间的长度。将这个二叉树视作与满二叉树结构相同，两端点间会出现一些延伸到这一层的
+ * null 节点，这些 null 节点也计入长度。
+ *
+ * 题目数据保证答案将会在  32 位 带符号整数范围内。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,3,2,5,3,null,9]
+ * 输出：4
+ * 解释：最大宽度出现在树的第 3 层，宽度为 4 (5,3,null,9) 。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,3,2,5,null,null,9,6,null,7]
+ * 输出：7
+ * 解释：最大宽度出现在树的第 4 层，宽度为 7 (6,null,null,null,null,null,7) 。
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = [1,3,2,5]
+ * 输出：2
+ * 解释：最大宽度出现在树的第 2 层，宽度为 2 (3,2) 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点的数目范围是 [1, 3000]
+ * -100 <= Node.val <= 100
+ *
+ *
+ *
+ *
+ */
+package lc662
+
+import "github.com/fakeYanss/leetcode-golang/helper"
+
+type TreeNode = helper.TreeNode
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func widthOfBinaryTree(root *TreeNode) int {
+	// return levelOrder(root)
+	return preOrder(root)
+}
+
+// 利用二叉堆的父子id特性
+
+// BFS 层序遍历
+type Entry struct {
+	node *TreeNode
+	id   int
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func levelOrder(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	maxWidth := 0
+	q := []Entry{}
+	q = append(q, Entry{root, 1})
+	// 从上到下整棵树
+	for len(q) > 0 {
+		sz := len(q)
+		start, end := 0, 0
+		// 从左到右遍历每一行
+		for i := 0; i < sz; i++ {
+			cur := q[0]
+			q = q[1:]
+
+			if i == 0 {
+				start = cur.id
+			}
+			if i == sz-1 {
+				end = cur.id
+			}
+
+			// 左右子节点压栈
+			if cur.node.Left != nil {
+				q = append(q, Entry{cur.node.Left, cur.id * 2})
+			}
+			if cur.node.Right != nil {
+				q = append(q, Entry{cur.node.Right, cur.id*2 + 1})
+			}
+		}
+		maxWidth = max(maxWidth, end-start+1)
+	}
+	return maxWidth
+}
+
+// DFS 先序遍历
+func preOrder(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	maxWidth := 1
+	// 记录最左侧节点的id
+	leftIds := []int{}
+	var traverse func(*TreeNode, int, int)
+	traverse = func(root *TreeNode, id, depth int) {
+		if root == nil {
+			return
+		}
+
+		if len(leftIds) == depth-1 {
+			// 第一次到达这个深度时，一定是最左侧节点
+			leftIds = append(leftIds, id)
+		} else {
+			maxWidth = max(maxWidth, id-leftIds[depth-1]+1)
+		}
+
+		traverse(root.Left, id*2, depth+1)
+		traverse(root.Right, id*2+1, depth+1)
+	}
+
+	traverse(root, 1, 1)
+	return maxWidth
+}
+
+// @lc code=end
