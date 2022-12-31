9d4367f73e464b91d847df158cce53c171f79e4a
fakeYanss <yanshisangc@gmail.com>
2022-12-31 16:14:06 +0800

    随便写 - 2 🚩

---
+++ leetcode/lc2/2.两数相加_test.go
@@ -0,0 +1,70 @@
+package lc2
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_addTwoNumbers(t *testing.T) {
+	type args struct {
+		l1 *ListNode
+		l2 *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "case1",
+			args: args{
+				l1: &ListNode{
+					Val: 9,
+					Next: &ListNode{
+						Val: 9,
+						Next: &ListNode{
+							Val:  9,
+							Next: nil,
+						},
+					},
+				},
+				l2: &ListNode{
+					Val: 9,
+					Next: &ListNode{
+						Val: 9,
+						Next: &ListNode{
+							Val: 9,
+							Next: &ListNode{
+								Val:  9,
+								Next: nil,
+							},
+						},
+					},
+				},
+			},
+			want: &ListNode{
+				Val: 8,
+				Next: &ListNode{
+					Val: 9,
+					Next: &ListNode{
+						Val: 9,
+						Next: &ListNode{
+							Val: 0,
+							Next: &ListNode{
+								Val:  1,
+								Next: nil,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addTwoNumbers(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("addTwoNumbers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
