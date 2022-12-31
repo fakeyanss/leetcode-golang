ee613b15a96da3eaead3e2d343cdbf3781a9d000
fakeYanss <yanshisangc@gmail.com>
2022-12-31 15:48:13 +0800

    每日一题 - 2037 🚩

---
+++ leetcode/lc2037/2037.使每位学生都有座位的最少移动次数_test.go
@@ -0,0 +1,31 @@
+package lc2037
+
+import "testing"
+
+func Test_minMovesToSeat(t *testing.T) {
+	type args struct {
+		seats    []int
+		students []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case1",
+			args: args{
+				seats:    []int{3, 1, 5},
+				students: []int{2, 7, 4},
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minMovesToSeat(tt.args.seats, tt.args.students); got != tt.want {
+				t.Errorf("minMovesToSeat() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
