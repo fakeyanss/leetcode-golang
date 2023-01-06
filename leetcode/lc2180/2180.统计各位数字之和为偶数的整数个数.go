b59ed83f55c813cc91262cf6526ad9d5114d3342
fakeYanss <yanshisangc@gmail.com>
2023-01-06 22:02:22 +0800

    每日一题 - 2180 🚩

---
+++ leetcode/lc2180/2180.统计各位数字之和为偶数的整数个数.go
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=2180 lang=golang
+ *
+ * [2180] 统计各位数字之和为偶数的整数个数
+ *
+ * https://leetcode.cn/problems/count-integers-with-even-digit-sum/description/
+ *
+ * algorithms
+ * Easy (68.12%)
+ * Likes:    60
+ * Dislikes: 0
+ * Total Accepted:    29.3K
+ * Total Submissions: 43K
+ * Testcase Example:  '4'
+ *
+ * 给你一个正整数 num ，请你统计并返回 小于或等于 num 且各位数字之和为 偶数 的正整数的数目。
+ *
+ * 正整数的 各位数字之和 是其所有位上的对应数字相加的结果。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：num = 4
+ * 输出：2
+ * 解释：
+ * 只有 2 和 4 满足小于等于 4 且各位数字之和为偶数。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：num = 30
+ * 输出：14
+ * 解释：
+ * 只有 14 个整数满足小于等于 30 且各位数字之和为偶数，分别是：
+ * 2、4、6、8、11、13、15、17、19、20、22、24、26 和 28 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= num <= 1000
+ *
+ *
+ */
+package lc2180
+
+// @lc code=start
+func countEven(num int) int {
+	cnt := 0
+	for i := 1; i <= num; i++ {
+		sum := 0
+		for x := i; x > 0; x /= 10 {
+			sum += x % 10
+		}
+		if sum%2 == 0 {
+			cnt++
+		}
+	}
+	return cnt
+}
+
+// @lc code=end
